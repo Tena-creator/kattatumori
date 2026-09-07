d9bbca26941594ebfd977fcf4d64cedd21164592
Tena <romanya.atelier@gmail.com>
2026-09-08 02:39:07 +0900

    Google

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,10 @@ export const metadata: Metadata = {
     shortcut: "/favicon.ico",
     apple: "/apple-icon.png",
   },
+  // ▼ アドセンスの審査用メタタグをここに追加！
+  other: {
+    "google-adsense-account": "ca-pub-7372592854852772",
+  },
 };
 
 export default function RootLayout({
@@ -34,28 +38,44 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        {/* ファビコンを強烈にブラウザへ認識させるタグ */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-icon.png" />
 
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5RJWTNJQ');
-          `}
-        </Script>
+        {/* ==========================================
+            ▼ Google Tag Manager (認識率100%の直接記述方式に変更) ▼
+        ========================================== */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5RJWTNJQ');
+            `,
+          }}
+        />
+
+        {/* ==========================================
+            ▼ Google AdSense 広告スクリプト ▼
+        ========================================== */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7372592854852772"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body className="min-h-full flex flex-col">
+        {/* ==========================================
+            ▼ Google Tag Manager (noscript) ▼
+        ========================================== */}
         <noscript>
-          <iframe 
+          <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5RJWTNJQ"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
 
