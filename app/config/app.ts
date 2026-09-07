bd79deda8379f4671039a6a5086338447a8dc924
Tena <romanya.atelier@gmail.com>
2026-09-08 00:26:27 +0900

    affiliate ID追加

--- app/config/app.ts
+++ app/config/app.ts
@@ -5,7 +5,7 @@ export const APP_CONFIG = {
   
   // ▼ アフィリエイト連携（ここに自分のIDを入れるだけでアプリ全体に反映されます！）
   affiliate: {
-    amazonTag: "kattatsumori-22", // AmazonアソシエイトのトラッキングID
-    rakutenId: "12345678.9abcdef0"  // 楽天アフィリエイトID
+    amazonTag: "kt031603-22", // AmazonアソシエイトのトラッキングID
+    rakutenId: "4fdcf039.cc5b3a58.4fdcf03a.d681d236"  // 楽天アフィリエイトID
   }
 };
